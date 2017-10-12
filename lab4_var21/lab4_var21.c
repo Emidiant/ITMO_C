6f08339a89f9b780460363490b6e80a0becefde8
Emidiant <32176800+Emidiant@users.noreply.github.com>
2017-10-12 20:37:48 +0300

    Add files via upload

---
+++ lab4_var21/lab4_var21.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+	scanf("%d",&x);
+	printf("%d\n", 10<=x && x<=100);
+    printf("0-false, 1-true\n");
+    
+    int y,z;
+    scanf("%o", &y);
+    z=y>>21;
+    z=z % 2;
+    printf("%d\n",z);
+    
+  
+    return 0;
+}
+
