b188bfd5becc11b8fbfffc250aceae64b2d4a976
Emidiant <32176800+Emidiant@users.noreply.github.com>
2017-10-12 20:38:30 +0300

    Update lab4_var21.c

--- lab4_var21/lab4_var21.c
+++ lab4_var21/lab4_var21.c
@@ -4,8 +4,8 @@
 int main()
 {
     int x;
-	scanf("%d",&x);
-	printf("%d\n", 10<=x && x<=100);
+    scanf("%d",&x);
+    printf("%d\n", 10<=x && x<=100);
     printf("0-false, 1-true\n");
     
     int y,z;
@@ -13,7 +13,6 @@ int main()
     z=y>>21;
     z=z % 2;
     printf("%d\n",z);
-    
   
     return 0;
 }
